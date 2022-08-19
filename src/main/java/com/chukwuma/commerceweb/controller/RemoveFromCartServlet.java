439e4c7e703b5cc5f342ca79009c37b7c225ec69
TheChukwuma <egwuonwuchukwuma74@gmail.com>
2022-08-19 12:00:55 +0100

    added remove button to the cart page

--- src/main/java/com/chukwuma/commerceweb/controller/RemoveFromCartServlet.java
+++ src/main/java/com/chukwuma/commerceweb/controller/RemoveFromCartServlet.java
@@ -16,8 +16,23 @@ public class RemoveFromCartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=UTF-8");
+        try(PrintWriter out = response.getWriter()){
+            int id = Integer.parseInt(request.getParameter("id"));
+            if (id != 0){
+                List<Cart> cart_list = (List<Cart>) request.getSession().getAttribute("cart-list");
+                if (!cart_list.isEmpty()){
+                    for (Cart cart : cart_list){
+                        if(cart.getId() == id){
+                            cart_list.remove(cart);
+                            break;
+                        }
+                    }
+                }response.sendRedirect("cart.jsp");
+            }else{
+                response.sendRedirect("Cart.jsp");
+        }
 
-
+        }
 
     }
 
